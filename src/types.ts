3b2fed855f7a5a6985316aebb78058b49e8bc84d
4Sight <116753476+4S1ght@users.noreply.github.com>
2023-02-06 23:25:21 +0100

    (+) Added typesOf() and allTruthy() methods

--- src/types.ts
+++ src/types.ts
@@ -23,10 +23,48 @@ export function typeOf(value: any, types: VariableType[]): boolean
 export function typeOf(value: any, types?: VariableType[]): VariableType|boolean {
     if (typeof types !== 'undefined') {
         if (Array.isArray(types)) return types.includes(typeOf(value))
-        throw TypeError(`typeOf(value, types) - "types" parameter was supplied but it's not an array. (type: ${typeof types})`)
+        throw TypeError(`typeOf(value, types[]) - "types" parameter was supplied but it's not an array. (type: ${typeof types})`)
     }
     if (Array.isArray(value)) return 'array'
     if (value === null) return 'null'
     return typeof value
 }
 
+
+/**
+ * Similar to `typeOf()`, it converts an array of items into an array of their types.  
+ * If a second `types[]` parameter is supplied then the returned value will be a boolean
+ * based on if all of the value types match at least one in the `types[]` parameter.
+ * Eg:
+ * ```js
+ * typesOf(['string', 12n]) // => ["string", "bigint"]
+ * typesOf(['string', 12n], ['string', 'number', 'null']) // => true
+ * typesOf([undefined], ['string', 'number', 'null']) // => false
+ * ```
+ * 
+ * @param values 
+ */
+export function typesOf(values: any[]): VariableType[]
+export function typesOf(values: any[], types: VariableType[]): boolean 
+export function typesOf(values: any[], types?: VariableType[]): VariableType[]|boolean {
+    if (typeof types !== 'undefined') {
+        if (Array.isArray(types)) {
+            const valueTypes = values.map(x => typeOf(x))
+            for (let i = 0; i < valueTypes.length; i++) 
+                if (!types.includes(valueTypes[i])) return false
+            return true
+        }
+        throw TypeError(`typesOf(values[], types[]) - "types" parameter was supplied but it's not an array. (type: ${typeof types})`)
+    }
+    return values.map(x => typeOf(x))
+}
+
+/**
+ * Returns true if all the provided items are truthy.
+ * @param items any
+ * @returns boolean 
+ */
+export function allTruthy(...items: any[]): boolean {
+    for (let i = 0; i < items.length; i++) if (!items[i]) return false
+    return true
+}
